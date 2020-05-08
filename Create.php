1de2b81ad0889e66f878d4414d83ad3db526abf2
Matt Bush <matt.bush@trustmytravel.com>
2020-05-08 14:30:20 +0200

    Extend create to allow for hashing additional values

--- Create.php
+++ Create.php
@@ -25,17 +25,18 @@ class Create
     /**
      * Generates the auth string.
      *
-     * @param  integer $channel_id       The ID of the channel the auth is for.
-     * @param  string  $channel_currency The base currency of the channel as a three letter ISO code.
-     * @param  integer $booking_total    The total of the booking in base currency as a cent value.
-     * @param  string  $channel_secret   The secret key for the channel the auth is for.
-     * @return string                    Hashed and salted auth key.
+     * @param  integer $channel_id        The ID of the channel the auth is for.
+     * @param  string  $channel_currency  The base currency of the channel as a three letter ISO code.
+     * @param  integer $booking_total     The total of the booking in base currency as a cent value.
+     * @param  string  $channel_secret    The secret key for the channel the auth is for.
+     * @param  array   $additional_fields Any additional fields to include in the auth string.
+     * @return string                     Hashed and salted auth key.
      */
-    public function getAuthstring($channel_id, $channel_currency, $booking_total, $channel_secret)
+    public function getAuthstring($channel_id, $channel_currency, $booking_total, $channel_secret, $additional_fields = [])
     {
         $this->setDateStamp();
 
-        $varString = $this->getVarString($channel_id, $channel_currency, $booking_total);
+        $varString = $this->getVarString($channel_id, $channel_currency, $booking_total, $additional_fields);
 
         $authString       = hash('sha256', $varString);
         $saltedAuthString = hash('sha256', $authString . $channel_secret);
@@ -49,17 +50,21 @@ class Create
      * @param  integer $channel_id         The ID of the channel the auth is for.
      * @param  string  $booking_currencies The base currency of the channel the auth is for as a three letter ISO code.
      * @param  integer $booking_total      The total of the booking in base currency as a cent value.
+     * @param  array   $additional_fields  Any additional fields to include in the auth string.
      * @return string                      Booking variables as a query string.
      */
-    public function getVarString($channel_id, $channel_currency, $booking_total)
+    public function getVarString($channel_id, $channel_currency, $booking_total, $additional_fields)
     {
-        $booking_vars = [
-            'channels'   => $channel_id,
-            'currencies' => $channel_currency,
-            'total'      => $booking_total,
-            'timestamp'  => $this->dateStamp,
-        ];
+        $additional_fields['channels']   = $channel_id;
+        $additional_fields['currencies'] = $channel_currency;
+        $additional_fields['total']      = $booking_total;
 
-        return implode('&', $booking_vars);
+        ksort($additional_fields);
+
+        $additional_fields['timestamp'] = $this->dateStamp;
+
+        return implode('&', array_map(function ($item) {
+            return is_array($item) ? json_encode($item) : $item;
+        }, $additional_fields));
     }
 }
